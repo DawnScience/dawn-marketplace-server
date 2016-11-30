9fd02060e11859f66d2beb7f62bc261d94b445c5
Baha El Kassaby <baha.elkassaby@gmail.com>
2016-11-30 16:28:15 +0000

    Update release build

--- org.dawnsci.marketplace.server/dawnmarket.sh
+++ org.dawnsci.marketplace.server/dawnmarket.sh
@@ -4,9 +4,22 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 SERVICE_NAME=DAWN_Market_Place_Server
 SERVER_LOG=dawnmarket.log
-PATH_TO_JAR=$DIR/org.dawnsci.marketplace.server-1.0.0-RELEASE.war
-PID_PATH_NAME=$DIR/dawn-marketplace-server-pid
+
 case $1 in
+    release)
+		JAR_NAME=org.dawnsci.marketplace.server-1.0.0-RELEASE.war
+    ;;
+    snapshot)
+		JAR_NAME=org.dawnsci.marketplace.server-1.0.0-SNAPSHOT.war
+    ;;
+    *)
+		echo "Invalid argument: $1"
+		exit 1
+esac
+
+PATH_TO_JAR=$DIR/$JAR_NAME
+PID_PATH_NAME=$DIR/dawn-marketplace-server-pid
+case $2 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
